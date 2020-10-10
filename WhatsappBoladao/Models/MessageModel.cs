b9ae2db75171d699ab53f482c313b013cf12b306
Daniel Amaro <daniel.amaro.rj@gmail.com>
2020-10-10 14:14:44 -0300

    commit inicial

---
+++ WhatsappBoladao/Models/MessageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace WhatsappBoladao.Models
+{
+    public class MessageModel
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+        public bool IsOwnMessage { get; set; }
+        public bool IsSystemMessage { get; set; }
+    }
+}
---
+++ WhatsappBolado/WhatsappBolado/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhatsappBolado.ViewModels;
+using Xamarin.Forms;
+
+namespace WhatsappBolado
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.BindingContext = new MainViewModel();
+            InitializeComponent();
+        }
+    }
+}
